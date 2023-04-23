651d6dea62a69b0eed3145957ff4511aa3fd52f3
eesuck1 <eesucksc@gmail.com>
2023-04-23 12:25:14 +0300

    Початковий коміт

---
+++ constants.py
@@ -0,0 +1,4 @@
+import pygame
+
+
+WIDTH, HEIGHT = 900, 500
---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import random
+
+import pygame
+
+
+class Game:
+    def __init__(self):
+        ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
