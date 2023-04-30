36b389348b27f335129dfcef9cf5d9991a5cb468
eesuck1 <eesucksc@gmail.com>
2023-04-30 13:07:29 +0300

    Merged player, enemy and bullet file into one game_objects file

--- Source/game.py
+++ Source/game.py
@@ -3,8 +3,7 @@ import sys
 import pygame
 
 from Source import constants
-from Source.enemy import Enemy
-from Source.game_objects import Player
+from Source.game_objects import Player, Enemy
 
 
 class Game:
@@ -13,11 +12,14 @@ class Game:
         self.__clock__ = pygame.time.Clock()
 
         self._player_ = Player((constants.SPRITE_WIDTH, constants.SPRITE_HEIGHT),
-                               ((constants.WIDTH - constants.SPRITE_WIDTH) // 2, constants.SPRITE_HEIGHT),
-                               constants.SPRITE_SPEED, constants.SPRITE_HEALTH, constants.PLAYER_IMAGE)
+                               ((constants.WIDTH - constants.SPRITE_WIDTH) // 2,
+                                constants.HEIGHT - 2 * constants.SPRITE_HEIGHT),
+                               constants.PLAYER_SPEED, constants.SPRITE_HEALTH, constants.PLAYER_IMAGE)
         self._player_.shoot()
 
         self._enemies_ = self.__init_enemies__(5)
+        for enemy in self._enemies_:
+            enemy.shoot()
 
         pygame.display.set_caption("Космоліт 5")
 
@@ -28,9 +30,12 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
+            self.__screen__.blit(constants.BACKGROUND_IMAGE, (0, 0))
             self.__draw_sprites__()
 
-            self.__screen__.blit(constants.BACKGROUND_IMAGE, (0, 0))
+            self.__move_objects()
+
+            self.__clock__.tick(constants.FPS)
             pygame.display.update()
 
     def __draw_sprites__(self) -> None:
@@ -39,15 +44,29 @@ class Game:
         for enemy in self._enemies_:
             enemy.draw(self.__screen__)
 
+            for bullet in enemy.get_bullets():
+                bullet.draw(self.__screen__)
+
         for bullet in self._player_.get_bullets():
             bullet.draw(self.__screen__)
+
+    def __move_objects(self) -> None:
+        self._player_.move(pygame.key.get_pressed())
+
+        for enemy in self._enemies_:
+            enemy.move()
+
+            for bullet in enemy.get_bullets():
+                bullet.move(1)
+
+        for bullet in self._player_.get_bullets():
+            bullet.move()
 
     @staticmethod
     def __init_enemies__(number_of_enemies: int) -> list[Enemy]:
         step = (constants.WIDTH - 2 * constants.SPRITE_WIDTH) // number_of_enemies
 
         return [Enemy((constants.SPRITE_WIDTH, constants.SPRITE_HEIGHT),
-                      (constants.SPRITE_WIDTH + step + index * constants.SPRITE_WIDTH,
-                       constants.HEIGHT - constants.SPRITE_HEIGHT),
-                      constants.SPRITE_SPEED, constants.SPRITE_HEALTH, constants.ENEMY_IMAGE) for index in
+                      (constants.SPRITE_WIDTH + step + index * constants.SPRITE_WIDTH, constants.SPRITE_HEIGHT),
+                      constants.ENEMY_SPEED, constants.SPRITE_HEALTH, constants.ENEMY_IMAGE) for index in
                 range(number_of_enemies)]
--- Source/game_objects.py
+++ Source/game_objects.py
@@ -5,7 +5,38 @@ from Source import constants
 from Source.sprite import Sprite
 
 
-class Player(Sprite):
+class Bullet(Sprite):
+    def __init__(self, start_coordinates: tuple[int, int], size: tuple[int, int], speed: int,
+                 color: tuple[int, int, int]):
+        super().__init__(size, start_coordinates, speed, 1)
+
+        self._color_ = color
+
+    def draw(self, surface: pygame.Surface) -> None:
+        """Implementation for bullet"""
+
+        pygame.draw.rect(surface, self._color_, self._rect_)
+
+    def move(self, direction=-1) -> None:
+        self.change_coordinates(y=direction * self._speed_)
+
+
+class ShootInterface(Sprite):
+    def __init__(self, size: tuple[int, int], start_coordinates: tuple[int, int], speed: int, health: int,
+                 image: pygame.Surface):
+        super().__init__(size, start_coordinates, speed, health, image)
+        self._bullets_ = []
+
+    def shoot(self) -> None:
+        if len(self._bullets_) <= 5:
+            self._bullets_.append(Bullet(self.get_coordinates(), (constants.BULLET_WIDTH, constants.BULLET_HEIGHT),
+                                         constants.BULLET_SPEED, (255, 255, 255)))
+
+    def get_bullets(self) -> list[Bullet]:
+        return self._bullets_
+
+
+class Player(ShootInterface):
     def __init__(self, size: tuple[int, int], start_coordinates: tuple[int, int], speed: int, health: int,
                  image: pygame.Surface):
         super().__init__(size, start_coordinates, speed, health, image)
@@ -14,38 +45,14 @@ class Player(Sprite):
     def move(self, key_pressed: pygame.key.ScancodeWrapper) -> None:
         if self.get_coordinates()[0] > 0 and key_pressed[pygame.K_a]:
             self.change_coordinates(x=-self._speed_)
-        if self.get_coordinates()[1] < constants.WIDTH - self._rect_.width and key_pressed[pygame.K_d]:
+        if self.get_coordinates()[0] < constants.WIDTH - self._rect_.width and key_pressed[pygame.K_d]:
             self.change_coordinates(x=self._speed_)
 
-    def shoot(self) -> None:
-        if len(self._bullets_) <= 5:
-            self._bullets_.append(Bullet((constants.BULLET_WIDTH, constants.BULLET_HEIGHT), 7, (255, 255, 255), self))
 
-    def get_bullets(self) -> list[Bullet]:
-        return self._bullets_
-
-
-class Enemy(Sprite):
+class Enemy(ShootInterface):
     def __init__(self, size: tuple[int, int], start_coordinates: tuple[int, int], speed: int, health: int,
                  image: pygame.Surface):
         super().__init__(size, start_coordinates, speed, health, image)
 
-
-class Bullet(Sprite):
-    def __init__(self, size: tuple[int, int], speed: int,
-                 color: tuple[int, int, int], shooter: Player | Enemy):
-        super().__init__(size, shooter.get_coordinates(), speed, 1)
-
-        self._shooter_ = shooter
-        self._color_ = color
-
-    def draw(self, surface: pygame.Surface) -> None:
-        """Implementation for bullet"""
-
-        pygame.draw.rect(surface, self._color_, self._rect_)
-
     def move(self) -> None:
-        if isinstance(self._shooter_, Player):
-            self.change_coordinates(y=-self._speed_)
-        if isinstance(self._shooter_, Enemy):
-            self.change_coordinates(y=self._speed_)
+        self.change_coordinates(y=self._speed_)
